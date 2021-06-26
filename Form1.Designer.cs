d597c316b4264c5736058597230fe9ff0c0de4e1
VladimirFant <86527100+VladimirFant@users.noreply.github.com>
2021-06-26 18:44:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Zmeyka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.point = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panelGameOver = new System.Windows.Forms.Panel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.poleGame = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panelGameOver.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.poleGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.splitContainer1.Panel1.Controls.Add(this.point);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonStart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.splitContainer1.Panel2.Controls.Add(this.panelGameOver);
+            this.splitContainer1.Panel2.Controls.Add(this.poleGame);
+            this.splitContainer1.Size = new System.Drawing.Size(1137, 676);
+            this.splitContainer1.SplitterDistance = 188;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // point
+            // 
+            this.point.AutoSize = true;
+            this.point.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point.Location = new System.Drawing.Point(3, 134);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(90, 32);
+            this.point.TabIndex = 1;
+            this.point.Text = "Очки:";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(3, 3);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(182, 49);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panelGameOver
+            // 
+            this.panelGameOver.BackColor = System.Drawing.Color.Gray;
+            this.panelGameOver.Controls.Add(this.buttonOK);
+            this.panelGameOver.Controls.Add(this.labelGameOver);
+            this.panelGameOver.Controls.Add(this.label1);
+            this.panelGameOver.Location = new System.Drawing.Point(278, 200);
+            this.panelGameOver.Name = "panelGameOver";
+            this.panelGameOver.Size = new System.Drawing.Size(361, 232);
+            this.panelGameOver.TabIndex = 1;
+            this.panelGameOver.Visible = false;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(144, 164);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 38);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "ОК";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGameOver.ForeColor = System.Drawing.SystemColors.Info;
+            this.labelGameOver.Location = new System.Drawing.Point(138, 101);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(90, 32);
+            this.labelGameOver.TabIndex = 0;
+            this.labelGameOver.Text = "Очки:";
+            this.labelGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(61, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Конец игры!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // poleGame
+            // 
+            this.poleGame.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.poleGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.poleGame.Location = new System.Drawing.Point(0, 0);
+            this.poleGame.Name = "poleGame";
+            this.poleGame.Size = new System.Drawing.Size(945, 676);
+            this.poleGame.TabIndex = 0;
+            this.poleGame.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 676);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Змейка";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panelGameOver.ResumeLayout(false);
+            this.panelGameOver.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.poleGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox poleGame;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label point;
+        private System.Windows.Forms.Panel panelGameOver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label labelGameOver;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Zmeyka
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Font font1 = new Font("Arial", 24, FontStyle.Bold);
+        Font font2 = new Font("Times New Roman", 18, FontStyle.Italic);
+        bool[,] pole;
+        Point [] hiro;
+
+        int kletka = 30,
+            tableI,
+            tableJ,
+            poleWidth,
+            poleHeight,
+            posI,
+            posJ,
+            posX = 0,
+            posY = 0,
+            points = 0;
+      
+        private void Form1_KeyDown(object sender, KeyEventArgs e) //привязка клавиш управления
+        {
+            e.SuppressKeyPress = true; // убирает БАГ звук восклицания при нажатии на клавиши
+            //запрет на нажатие других клавиш
+            if (e.KeyCode != Keys.D && e.KeyCode != Keys.A && e.KeyCode
+                != Keys.S && e.KeyCode != Keys.W) return;
+            // запрет на обратное движение
+            if ((posX == 1 && e.KeyCode == Keys.A) ||
+                (posX == -1 && e.KeyCode == Keys.D) ||
+                (posY == 1 && e.KeyCode == Keys.W) ||
+                (posY == -1 && e.KeyCode == Keys.S)) return;
+
+            posX = 0; posY = 0;
+            
+            if (e.KeyCode == Keys.D) posX = 1;
+            if (e.KeyCode == Keys.A) posX = -1;
+            if (e.KeyCode == Keys.S) posY = 1;
+            if (e.KeyCode == Keys.W) posY = -1;
+            
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            panelGameOver.Visible = false;
+            drawSetup();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            drawPole();
+            timer1.Start();
+            posX = 1;
+            buttonStart.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // таймер движения и отрисовки
+        {
+            proverkaNaPopadanieVCel();
+           proverkaNaVyhodZaPredelyMassiva();
+            zmey();
+            drawPole();
+            proverkaNaNaezdNaHvost();
+        }
+
+       void proverkaNaPopadanieVCel()
+        {
+            if (hiro[0].X + posX == posI && hiro[0].Y + posY == posJ)
+            {
+                Array.Resize(ref hiro, hiro.Length + 1);
+                points++;
+                point.Text = "Очки: " + points;
+                pole[posI, posJ] = false;
+                randomCell();
+            }
+        }
+
+        void proverkaNaVyhodZaPredelyMassiva()
+        {
+            if (hiro[0].X == -1)
+            {
+                hiro[0].X = tableI;
+                return;
+            }
+                
+            if (hiro[0].X == tableI)
+            {
+                hiro[0].X = 0;
+                return;
+            }
+            if (hiro[0].Y == -1)
+            {
+                hiro[0].Y = tableJ;
+                return;
+            }
+
+            if (hiro[0].Y == tableJ)
+            {
+                hiro[0].Y = 0;
+                return;
+            }
+
+        }
+
+        void proverkaNaNaezdNaHvost()
+        {
+            for(int i = 1; i < hiro.Length; i++)
+            {
+                if(hiro[0].X == hiro[i].X && hiro[0].Y == hiro[i].Y)
+                {
+                    timer1.Stop();
+                    labelGameOver.Text = "Очки: " + points;
+                    panelGameOver.Visible = true;
+                }
+            }
+        }
+        public Form1() // инициализация и первичная отрисовка
+        {
+            InitializeComponent();
+            this.KeyPreview = true; //подключаем кнопки клавиатуры
+            drawSetup();
+        }
+
+        void drawSetup() // начальные установки и отрисовка исходного поля
+        {
+            poleWidth = poleGame.Width - (poleGame.Width%kletka);
+            poleHeight = poleGame.Height - (poleGame.Width % kletka);
+            poleGame.Width = poleWidth;
+            poleGame.Height = poleHeight;
+            tableI = poleWidth / kletka;
+            tableJ = poleHeight / kletka;
+            pole = new bool[tableI, tableJ];
+            hiro = new Point[1];
+            posX = 0;
+            posY = 0;
+            points = 0;
+            point.Text = "Очки: 0";
+
+            randomCell();
+            poleGame.Image = new Bitmap(poleWidth, poleHeight);
+            graphics = Graphics.FromImage(poleGame.Image);
+            string str = "Игра ЗМЕЙКА";
+            SizeF size = graphics.MeasureString(str, font1);
+            graphics.DrawString(str, font1, Brushes.Black, poleGame.Width / 2 - Convert.ToInt32(size.Width) / 2, poleGame.Height / 2 - Convert.ToInt32(size.Height) / 2);
+            str = "© StudioFant 2021";
+            size = graphics.MeasureString(str, font2);
+            graphics.DrawString(str, font2, Brushes.Black, poleGame.Width / 2 - Convert.ToInt32(size.Width) / 2, poleGame.Height / 2 + Convert.ToInt32(size.Height));
+            buttonStart.Enabled = true;
+        }
+
+        void zmey () // создание змея. построение массива значений
+        {
+            int x = hiro[0].X + posX; int y = hiro[0].Y + posY;
+            for (int i = hiro.Length - 1; i >= 1; i--)
+            {
+                hiro[i].X = hiro[i - 1].X; hiro[i].Y = hiro[i - 1].Y;
+            }
+            hiro[0].X = x; hiro[0].Y = y;
+        }
+
+        void randomCell () // генератор случайного расположения цнли для змейки
+        {
+            Random random = new Random();
+            bool flag = true;
+            while (flag)
+            { flag = false;
+                posI = random.Next(0, tableI - 1);
+                posJ = random.Next(0, tableJ - 1);
+                for (int i = 0; i < hiro.Length; i++)
+                {
+                    if (posI == hiro[i].X && posJ == hiro[i].Y)
+                    {
+                        flag = true;
+                        break;  
+                    }
+                }
+                
+            }
+
+            pole[posI, posJ] = true;
+        }
+
+        void drawPole() // отрисовка игрового поля по тику таймера
+        {
+            graphics.Clear(Color.LightCoral);
+            for(int i = 0; i < tableI; i++)
+            {
+                for(int j = 0; j < tableJ; j++)
+                {
+                    graphics.DrawRectangle(Pens.Black, i*kletka, j*kletka, kletka, kletka);
+                    if (pole[i,j]) graphics.FillEllipse(Brushes.Yellow, i * kletka, j * kletka, kletka, kletka);
+                }
+            }
+            for(int i = 0; i < hiro.Length; i++)
+            {
+                {
+                    graphics.FillRectangle(Brushes.Blue, hiro[i].X * kletka, hiro[i].Y * kletka, kletka, kletka);
+                }
+            }
+            poleGame.Refresh();
+            //GC.Collect(); // сборщик мусора принудительное включение
+        }
+
+        
+    }
+}
